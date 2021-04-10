b2311bae726989ebc30da755fc5cc4a499795a05
Nishat Hoque <hoquen0397@gmail.com>
2021-04-10 18:49:01 -0400

    variables

--- variables.py
+++ variables.py
@@ -13,3 +13,28 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+#no semi-colons, variables do not need var, const, let 
+x = 1             # casted as an int by default
+y = 2.5           # float
+name = 'John'     # str - string 
+is_cool = True    # bool - for booleans you need a capital T or F, otherwise it'll be looked as a variable
+
+#Multiple Assignment 
+x, y, name, is_cool = (1, 2.5, 'John', True) #same code as above 
+
+#Basic Math 
+a = x + y
+
+#Check Type
+print(type(x))
+
+# Casting
+x = str(x)
+y = int(y)
+print(type(x))
+print(type(y), y) # 2.5 => will go to 2 
+z = float(y)
+print(type(z),z) # 2 ==> will go to 2.0, will not go back to 2.5 because y is no going from int 2 to float 2.0 
+
+print (x, y, name, is_cool, a)
