84e758d31e94311051a866a7d38bde44103f5d84
Nishat Hoque <hoquen0397@gmail.com>
2021-04-16 18:07:52 -0400

    madlib

---
+++ beginner_projects/madlib_gen.py
@@ -0,0 +1,12 @@
+# skills used : string concatenation and f strings.
+
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+person_name = input('Name: ')
+place = input('Famous Place: ')
+
+madLib = f'Computer programming is so {adj}! This is so exciting. My favorites things to do are {verb1} and {verb2}. \
+My favorite person is {person_name}. My favorite place is {place} '
+
+print(madLib)
